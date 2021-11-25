39fde126a36ee0978c029a30d05182427f8a3941
elguillen <elguillen@users.noreply.github.com>
2021-11-24 20:44:02 -0800

    Add Navigation Graph and create the Navigation Actions to navigate between the fragments.

--- app/src/main/java/com/chiu/moneytracks/MenuListFragment.kt
+++ app/src/main/java/com/chiu/moneytracks/MenuListFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.chiu.moneytracks.databinding.FragmentMenuListBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +23,9 @@ class MenuListFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private var _binding: FragmentMenuListBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,8 +38,9 @@ class MenuListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentMenuListBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_list, container, false)
+        return binding.root
     }
 
     companion object {
@@ -55,5 +61,29 @@ class MenuListFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.incomeButton.setOnClickListener {
+            val action = MenuListFragmentDirections.actionMenuListFragmentToIncomeFragment()
+            this.findNavController().navigate(action)
+        }
+
+        binding.expensesButton.setOnClickListener {
+            val action = MenuListFragmentDirections.actionMenuListFragmentToExpensesFragment()
+            this.findNavController().navigate(action)
+        }
+
+        binding.resultsButton.setOnClickListener {
+            val action = MenuListFragmentDirections.actionMenuListFragmentToResultsFragment()
+            this.findNavController().navigate(action)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
