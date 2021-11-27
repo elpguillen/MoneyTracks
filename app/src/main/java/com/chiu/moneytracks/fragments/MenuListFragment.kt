c2f5700bbd22899b2e4abee26a49dfb4b0e9ffe3
elguillen <elguillen@users.noreply.github.com>
2021-11-27 15:17:08 -0800

    Add Fragment that will add new expenses/gains and add the fragment to the navigation graph. Implemented the base layout for each fragment.

--- app/src/main/java/com/chiu/moneytracks/fragments/MenuListFragment.kt
+++ app/src/main/java/com/chiu/moneytracks/fragments/MenuListFragment.kt
@@ -1,4 +1,4 @@
-package com.chiu.moneytracks
+package com.chiu.moneytracks.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,30 +8,19 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.chiu.moneytracks.databinding.FragmentMenuListBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [MenuListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 class MenuListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private var _binding: FragmentMenuListBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -41,26 +30,6 @@ class MenuListFragment : Fragment() {
         _binding = FragmentMenuListBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         return binding.root
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MenuListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MenuListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -80,6 +49,12 @@ class MenuListFragment : Fragment() {
             val action = MenuListFragmentDirections.actionMenuListFragmentToResultsFragment()
             this.findNavController().navigate(action)
         }
+
+        binding.addIncomeItemButton.setOnClickListener {
+            val action = MenuListFragmentDirections.actionMenuListFragmentToAddItemFragment()
+            this.findNavController().navigate(action)
+        }
+
     }
 
     override fun onDestroy() {
